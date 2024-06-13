61170cc5dc855c9cafa4d02baa16eafbfaf296be
jaydixit088 <jay.dixit_bca22@gla.ac.in>
2024-06-13 22:00:45 +0530

    first commit

---
+++ TicTacToe.java
@@ -0,0 +1,99 @@
+import java.util.Scanner;
+public class TicTacToe {
+    private char[][] board;
+    private char currentPlayer;
+    public TicTacToe() {
+        board = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+        currentPlayer = 'X';
+    }
+
+    public void displayBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean isValidMove(int row, int col) {
+        return row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == '-';
+    }
+
+    public boolean makeMove(int row, int col) {
+        if (isValidMove(row, col)) {
+            board[row][col] = currentPlayer;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean hasWon() {
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) {
+                return true;
+            }
+        }
+
+        // Check columns
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if ((board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) ||
+                (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean isTie() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void playGame() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            displayBoard();
+            System.out.println("Player " + currentPlayer + "'s turn. Enter row and column (0-2):");
+            int row = scanner.nextInt();
+            int col = scanner.nextInt();
+            if (makeMove(row, col)) {
+                if (hasWon()) {
+                    displayBoard();
+                    System.out.println("Player " + currentPlayer + " wins!");
+                    break;
+                } else if (isTie()) {
+                    displayBoard();
+                    System.out.println("It's a tie!");
+                    break;
+                }
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            } else {
+                System.out.println("Invalid move. Try again.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        game.playGame();
+    }
+}
